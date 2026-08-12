74729ff4b5f9ffb8df0d7bb7715a6d25cdc5d371
Dan Taylor <dantaylor08@gmail.com>
2026-08-12 09:55:13 -0400

    Report builder beta build

--- app/api/quill/route.ts
+++ app/api/quill/route.ts
@@ -6,7 +6,7 @@ export async function POST(request: Request) {
     privateKey: process.env.QUILL_PRIVATE_KEY!,
     databaseType: DatabaseType.postgresql,
     databaseConnectionString: process.env.DATABASE_URL!,
-    metadataServerURL: 'http://localhost:8080',
+    metadataServerURL: 'https://quill-server-pr-468.fly.dev',
   });
 
   if (!quill) {
